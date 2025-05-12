ab7aee1a38f7605fc76cebb94e7833d5be877ac5
Foll0wTh3Wh1teRabbit <n.kosarev@g.nsu.ry>
2025-05-12 16:30:20 +0700

    Initial Commit, environment and launch configurations

---
+++ task-planner/src/main/java/ru/nsu/planner/TaskPlannerApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPlannerApplication.class, args);
+    }
+
+}
